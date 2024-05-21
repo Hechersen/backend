ff560cd99b688238ba33036455da8df656bba541
Héctor Hernández <143550206+Hechersen@users.noreply.github.com>
2024-05-21 10:49:49 -0300

    Correcciones cuarta entrega desarrollo backend Coderhouse

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const { engine } = require('express-handlebars');
-const fs = require('fs');
-const path = require('path');
+const ProductManager = require('./models/productManager');
+const productManager = new ProductManager('./data/products.json');
 
 const app = express();
 const server = http.createServer(app);
@@ -18,45 +18,32 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-const productsFilePath = path.join(__dirname, 'data', 'products.json');
-
-const readProducts = () => {
-  try {
-    const data = fs.readFileSync(productsFilePath, 'utf8');
-    return JSON.parse(data);
-  } catch (error) {
-    return [];
-  }
-};
-
-const saveProducts = (products) => {
-  fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2), 'utf8');
-};
-
 const productRoutes = require('./routes/products');
 const cartRoutes = require('./routes/carts');
 app.use('/api/products', productRoutes);
 app.use('/api/carts', cartRoutes);
 
+app.get('/realtimeproducts', async (req, res) => {
+  const products = await productManager.getAllProducts();
+  res.render('realTimeProducts', { products });
+});
+
 io.on('connection', (socket) => {
   console.log('Usuario conectado');
 
-  socket.on('new product', (product) => {
-    const products = readProducts();
+  socket.on('new product', async (product) => {
+    const products = await productManager.getAllProducts();
     const existingProduct = products.find(p => p.code === product.code);
     if (existingProduct) {
       socket.emit('error', 'Product with this code already exists');
     } else {
-      products.push(product);
-      saveProducts(products);
-      io.emit('product update', product);
+      const newProduct = await productManager.addProduct(product);
+      io.emit('product update', newProduct);
     }
   });
 
-  socket.on('delete product', (productId) => {
-    const products = readProducts();
-    const filteredProducts = products.filter(p => p.id !== productId);
-    saveProducts(filteredProducts);
+  socket.on('delete product', async (productId) => {
+    await productManager.deleteProduct(parseInt(productId));
     io.emit('product delete', productId);
   });
 
--- readme.txt
+++ readme.txt
@@ -1,10 +1,8 @@
-Cuarta entrega del curso de desarrollo backend de Coderhouse. (Primera pre entrega)
+Correcciones de la cuarta entrega del curso de desarrollo backend de Coderhouse.
 
-Realicé la corrección realizada en la entrega sobre el metodo addProductToCart.
-Instale las nuevas dependencias para este desafío.
-Cree los archivos con el código.
-Probé la viste del navegador desde http://localhost:8080/api/products
-Por ultimo intente agregar un cart, productos e insertar estos productos en el cart. Todo parecía funcionar.
+Realicé las correcciones realizadas en la entrega del desafío.
+Cambie la configuracion de los socket.
+Me asegure de que la vista de Handlebars funcione y se puedan agregar y quitar productos en tiempo real
 
 
 Muchas gracias!
